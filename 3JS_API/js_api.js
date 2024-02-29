19b652406567d72bbfc58427c657f871ffcc2ec7
Saisumanthv <104812692+Saisumanthv@users.noreply.github.com>
2024-02-29 18:29:19 +0530

    0.2

--- 3JS_API/js_api.js
+++
@@ -1,7 +0,0 @@
-fetch("js_api.wasm")
-    .then(response => response.arrayBuffer())
-    .then(bytes => WebAssembly.instantiate(bytes))
-    .then(response => {
-        let result = response.instance.exports.add(89,2);
-        console.log(result);
-    });
--- 3JS_API/js_api.wat
+++
@@ -1,8 +0,0 @@
-(module
-    (func $add (param i32) (param $p1 i32) (result i32)
-        local.get 0
-        local.get $p1
-        i32.add
-    )
-    (export "add" (func $add))
-)
